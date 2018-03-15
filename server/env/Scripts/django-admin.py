e02736034730efcd8754a6fbbb18645402e8f215
hyunbumy <hyunbumy@usc.edu>
2018-03-15 06:36:13 -0700

    simple test server for rpi endpoint

---
+++ server/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\didgu\onedrive\desktop\school\projects\remote_key\server\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
