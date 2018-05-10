727208d455f50fde499f4df1b7f80714ea8d104e
Hyun-Bum Yang <hyunbumy@usc.edu>
2018-05-10 11:24:32 -0700

    Untrack changes for venv

--- server/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\didgu\onedrive\desktop\school\projects\remote_key\server\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
