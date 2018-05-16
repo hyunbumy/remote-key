22297bbaebc9da266afdbcaeb9353173bbf26dd8
Hyun-Bum Yang <hyunbumy@usc.edu>
2018-05-16 00:56:52 -0700

    Change to OpenClose to APIView

--- server/simpleserver/locker/views.py
+++ server/simpleserver/locker/views.py
@@ -1,22 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpRequest
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from locker.apps import isOpen
 
 #import RPI.GPIO as GPIO
 
 # Create your views here.
-class OpenCloseView(View):
+class OpenCloseView(APIView):
     # request has the arguments from the client
     # need to parse the request and make a context to pass into the html render
     # template uses the passed in context (dictionary) to populate its data
 
     # If GET, simply show the page
-    def get(self, request):
-        return render(request,'locker/test.html')
+    def get(self, request, format=None):
+        return Response("GET")
 
     # If POST, then open / close
-    def post(self, request):
+    def post(self, request, format=None):
         #TODO: Find a way to store state info of the lock
         global isOpen
         if isOpen:
@@ -25,4 +24,4 @@ class OpenCloseView(View):
         else:
             print("Opening")
             isOpen = True
-        return render(request,'locker/test.html')
+        return Response(isOpen)
