5735dc460ddf0228548fdecfc75e3c77ccc5b49a
Hyun-Bum Yang <hyunbumy@usc.edu>
2018-05-17 04:39:07 -0700

    Connect to local mysql server

--- server/simpleserver/simpleserver/settings.py
+++ server/simpleserver/simpleserver/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'simpleserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'remote_key',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
