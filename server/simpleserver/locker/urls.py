15a47c0ae029cd438bd0191f74168ff41e88e934
hyunbumy <hyunbumy@usc.edu>
2018-06-01 07:02:13 -0700

    Change relative import reference to absolute

--- server/simpleserver/locker/urls.py
+++ server/simpleserver/locker/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
+from locker import views
 
 urlpatterns = [
     path('', views.OpenCloseView.as_view(), name='open'),
