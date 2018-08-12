cddc757403170412e4f815c874965e29db7a9e28
hyunbumy <hyunbumy@usc.edu>
2018-08-12 10:15:56 -0700

    Add CORS enabler

--- server/simpleserver/simpleserver/settings.py
+++ server/simpleserver/simpleserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5m7c27m&u&lgqzsm8&jrju(lq$)$7k=fl6311-u*cay(xsuson'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.123.109']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'locker.apps.LockerConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
